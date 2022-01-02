24952e148bdd0714d5c9f7643baa5892e6d8e2f5
DeLuc <lucas.g.pahlke@gmail.com>
2022-01-02 14:01:27 +0100

    create test cases for uittest

--- unittest2/unittest_testsuite.py
+++ unittest2/unittest_testsuite.py
@@ -15,5 +15,24 @@ class TestStringMethods(unittest.TestCase):
         with self.assertRaises(TypeError):
             s.split(2)
 
+class TestDictionarys(unittest.TestCase):
+    dictionary = {"Correct":1, " Wrong":2, " No match":4, "This matchs":3}
+    keylist = ["Correct", "This matchs", "Non existing"]
+
+    def test_key_format(self):
+        for key in self.dictionary.keys():
+
+            with self.subTest(key = key):
+
+                self.assertRegex(key, '^[^\s]+[a-zA-Z0-9]+')
+
+    def test_key_existing(self):
+        dict_keys =  self.dictionary.keys()
+
+        for key in self.keylist:
+            with self.subTest(key = key):
+                self.assertIn(key, dict_keys)
+
+
 if __name__ == '__main__':
     unittest.main()
